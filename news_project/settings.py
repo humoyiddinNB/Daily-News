fe2d8dbdc76ace195ee305b8e0771b5264c4e12e
N'ematillayev Humoyiddin <defendereviver71@gmail.com>
2025-03-27 21:50:34 +0500

    Extra changes added

--- news_project/settings.py
+++ news_project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static/']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_FINDERS = [
@@ -140,7 +140,7 @@ STATICFILES_FINDERS = [
 
 import os
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
